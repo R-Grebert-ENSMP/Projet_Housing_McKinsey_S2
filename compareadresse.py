2998ec9d6f0889b4deb064c32113c83f27ce7590
cohennion <56261575+cohennion@users.noreply.github.com>
2020-04-21 14:00:33 +0200

    comparateur d'adresse modifié

--- compareadresse.py
+++ compareadresse.py
@@ -1,23 +1,28 @@
 import sys
 import numpy as np
 import jellyfish
-import nltk
-from nltk.corpus import stopwords
+#import nltk
+#from nltk.corpus import stopwords
 
 
-#test1 = 'GEN LEMONNIER'
-#test2= "du GÃ©nÃ©ral Lemonnier"
+test1 = 'GEN LEMONNIER'
+test2= "du GÃ©nÃ©ral Lemonnier"
 
-def accent(string):
-    #remplace les mauvais caractère de la table valeurfoncière
-    old = ['Ã©','Ã¨','Ã´','Ã¢','Ã»',"Ãª","'"]
-    new = ['e','e','o','a','u','e',' ']
-    for i in range(len(old)):
-        string = str.replace(string, old[i], new[i])
+def normalisation(string):
+    #remplace les mauvais caractères de la table valeurfoncière
+    oldaccent = ['Ã©','Ã¨','Ã´','Ã¢','Ã»',"Ãª"]
+    newaccent = ['e','e','o','a','u','e']
+    for i in range(len(oldaccent)):
+        string = str.replace(string, oldaccent[i], newaccent[i])
+    string = string.lower()
+    oldabr = ["'"," de ", " du "," de la "," des ", " d'", " de l'"," les "," le "," la ", "general","place","impasse","docteur","saint","route","boulevard","avenue","allee","sentier","chemin","lotissement","passage","promenade"]
+    newabr = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','gen','pl',"imp","doc","st","ret","bd","av","all","sen","che","lot","pas","prom"]
+    for i in range(len(oldabr)):
+        string = str.replace(string, oldabr[i], newabr[i])
     return string
 
-def deletewords(string):
-    #supprime les mots tels que "de" "la" "du"... pour conparer plus facilement
+#def deletewords(string):
+    #supprime les mots tels que "de" "la" "du"... pour comparer plus facilement
     sw = list(stopwords.words('french'))
     listwords = str.split(string)
     i = 0
@@ -29,25 +34,22 @@ def deletewords(string):
     string = " ".join(listwords)
     return string
 
-def normalisation_adresse(voie) : 
+#def normalisation_adresse(voie) : 
     voie = accent(voie)
     voie = voie.lower()
     voie = deletewords(voie)
     return voie
 
 def compare(num1, type1, voie1, num2, type2, voie2):
-    voie1 = normalisation_adresse(voie1) #normalise l'écriture, minuscule sans accent, sans apostrophe, sans stopwords
-    voie2 = normalisation_adresse(voie2)
-    distance = jellyfish.jaro_distance(normalisation_adresse(voie1), normalisation_adresse(voie2))
-    if num1==num2 and type1==type2 and distance > 0.8 : #possibilité de rajouter une sélection sur la distance minimale
+    voie1 = normalisation(" " + voie1) #normalise l'écriture, minuscule sans accent, sans apostrophe, sans stopwords
+    voie2 = normalisation(" " + voie2)
+    distance = jellyfish.jaro_distance(voie1, voie2)
+    if num1==num2 and type1==type2 and distance > 0.9 : #possibilité de rajouter une sélection sur la distance minimale
         return True
     else : 
         return False
 
-#print(compare(4, 'rue', test1 , 4, 'rue', test2))
+print(compare(4, 'rue', test1 , 4, 'rue', test2))
 
-
-
-
-
-
+#distance = jellyfish.jaro_distance(normalisation(" " + test1), normalisation(" " + test2))
+#print(normalisation(" " + test1),normalisation(" " + test2),distance)

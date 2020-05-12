81dc64643f07cdf1b3a56e92190076e8b6b29d43
R-Grebert-ENSMP <55084314+R-Grebert-ENSMP@users.noreply.github.com>
2020-05-12 17:21:09 +0200

    nodes completed with merger clean and references to the new global_variables with "parameters" corrected in nodes

--- immobilier-mckinsey/src/immo/pipelines/data_engineering/nodes.py
+++ immobilier-mckinsey/src/immo/pipelines/data_engineering/nodes.py
@@ -50,7 +50,7 @@ def normalisation(str_err):
     """
     # Replace accents related errors
     str_accents = str_err
-    for encod_err, encod_corr in norm_accents.items():
+    for encod_err, encod_corr in parameters["norm_accents"].items():
         str_accents = str_accents.replace(encod_err, encod_corr)
     # Lowercase the string
     str_lower = str_accents.lower()
@@ -59,12 +59,12 @@ def normalisation(str_err):
         [
             word
             for word in str_lower.split(" ")
-            if (word not in stop_words) and (len(word) > 0)
+            if (word not in parameters["stop_words"]) and (len(word) > 0)
         ]
     )
     # Abbreviate common names
     str_abbreviate = str_stopwords
-    for long_name, abbr_name in norm_abbrevations.items():
+    for long_name, abbr_name in parameters["norm_abbrevations"].items():
         str_abbreviate = str_abbreviate.replace(long_name, abbr_name)
     return str_abbreviate
 
@@ -108,6 +108,63 @@ def compare(
     else:
         return False
 
+# ------------------- Merger -------------------#
+def merger(clean_cadastre, clean_valeur_fonc, columns_merger, columns_cadastre_compare, columns_vf_compare):
+
+'''
+This fucntion is the final step, it takes the clean table of  cadastre as the scafold, it also needs the name of the columns that need to be compared in both and the list of columns that we want to add to the scafold from valeur fonciere. Then it compares the data and everytime it gets a match, the data from valeur fonciere is added to the scafold.
+Args:
+    clean_cadastre (pandas dataframe): the cleaned table of cadastre
+    clean_valeur_fonciere (pandas dataframe): the cleaned table of valeur fonciere
+    columns_merger (list): list of columns to add to the clean cadastre
+    columns_compare (list): list of columns to compare between the two sets
+Returns:
+    master_table_f (pandas dataframe): the final master table with both sets merged
+'''
+
+
+    master_table = clean_cadastre.copy() #Copying of cadastre
+    long_columns_merger = len(columns_merger)
+    for i in columns_merger:
+        master_table[i] = np.NaN
+
+    M1 = np.array(clean_cadastre)
+    M2 = np.array(clean_valeur_fonc) # Passing to numpy
+    R = np.array(master_table)
+    columns_clean_cadastre = list(clean_cadastre.columns)
+    columns_clean_valeur_fonc = list(clean_valeur_fonc.columns)
+    columns_master_table = list(master_table.columns)
+
+    indexem_columns_clean_valeur_fonc = [columns_clean_valeur_fonc.index(i) for i in columns_merger] # Indexes of the columns in valeur_fonciere to merge
+
+    indexem_columns_master_table = [columns_master_table.index(i) for i in columns_merger] # Indexes of the columns in master_table to merge
+
+    #Those list are necessary to work with numpy instead of pandas
+    indexec_columns_clean_cadastre = [columns_clean_cadastre.index(i) for i in columns_cadastre_compare] # Indexes of the columns in cadastre that we have to compare
+
+    indexec_columns_clean_valeur_fonc = [columns_clean_valeur_fonc.index(i) for i in columns_vf_compare] # Indexes of the columns in valeur_fonciere that we have to compare
+
+    a = len(M1[:,0])
+    b = len(M1[:,0])
+
+
+#Since we are basing our comparison on the number, street type and street, I created a tuple with those 3 infos in it, this tuple needs to be modified if we are to compare other informations between the two sets
+
+    for i in range (a):
+        (Num1, Type1, Voie1) = (M1[i, p] for p in indexec_columns_clean_cadastre)
+
+        for j in range (b):
+            (Num2, Type2, Voie2) = (M2[j, p] for p in indexec_columns_clean_cadastre)
+
+            if compare(Num1, Type1, Voie1, Num2, Type2, Voie2):
+
+                for k in range (long_columns_merger):
+                    R[i, indexem_columns_master_table[k]] = M2[j, indexem_columns_clean_valeur_fonc[k]]
+
+    master_table_f = pd.DataFrame(R)
+    master_table_f.columns = master_table.columns
+
+    return master_table_f
 
 # ------------------- Cleaning Functions ------------------- #
 
@@ -128,13 +185,13 @@ def sep_voies(cadastre_df):
     """
     # The pd.DataFrame.apply() method is the best to apply any type of function
     # It prevents from looping on pd df, which is very costly
-    cadastre_df[cad_street_type] = cadastre_df[cad_street_full].apply(
+    cadastre_df[parameters["cad_street_type"]] = cadastre_df[parameters["cad_street_full"]].apply(
         lambda x: x.split(" ")[0]
     )
-    cadastre_df[cad_street_name] = cadastre_df[cad_street_full].apply(
+    cadastre_df[parameters["cad_street_name"]] = cadastre_df[parameters["cad_street_full"]].apply(
         lambda x: " ".join(x.split(" ")[1:])
     )
-    return cadastre_df.drop(cad_street_full, axis=1)
+    return cadastre_df.drop(parameters["cad_street_full"], axis=1)
 
 
 def corr_type_de_voie_vf(valeur_fonc_df):
@@ -148,8 +205,8 @@ def corr_type_de_voie_vf(valeur_fonc_df):
     Returns:
         pandas dataframe: the same dataframe, with standardized values in the street_type column
     """
-    valeur_fonc_df[vf_street_type] = valeur_fonc_df[vf_street_type].apply(
-        lambda x: norm_street_types[x] if x in norm_street_types.keys() else x
+    valeur_fonc_df[parameters["vf_street_type"]] = valeur_fonc_df[parameters["vf_street_type"]].apply(
+        lambda x: parameters["norm_street_types[x]"] if x in parameters["norm_street_types"].keys() else x
     )
     return valeur_fonc_df
 
@@ -197,8 +254,8 @@ def get_square_meter_price(valeur_fonc_df):
     Returns:
         pandas dataframe: the same dataframe, with the new vf_square_meter_price column.
     """
-    valeur_fonc_df[vf_square_meter_price] = (
-        valeur_fonc_df[vf_price_nominal] / valeur_fonc_df[vf_built_area]
+    valeur_fonc_df[parameters["vf_square_meter_price"]] = (
+        valeur_fonc_df[parameters["vf_price_nominal"]] / valeur_fonc_df[parameters["vf_built_area"]]
     ).round()
     # TO DO : catch the surface == 0 exception
     return valeur_fonc_df

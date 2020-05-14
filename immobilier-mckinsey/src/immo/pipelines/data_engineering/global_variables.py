45257de4bd1339898d3fd1c90d771890d8cb08b5
cohennion <56261575+cohennion@users.noreply.github.com>
2020-05-14 16:07:20 +0200

    column name correction

--- immobilier-mckinsey/src/immo/pipelines/data_engineering/global_variables.py
+++ immobilier-mckinsey/src/immo/pipelines/data_engineering/global_variables.py
@@ -5,8 +5,8 @@ parameters = {
 # Column Names
 # Valeurs Foncieres table
 "vf_street_type" : "Type de voie",
-"vf_price_nominal" : "Valeur Fonciere",
-"vf_built_area" : "Surface reelle batie",
+"vf_price_nominal" : "Valeur fonciere",
+"vf_built_area" : "Surface reelle bati",
 "vf_square_meter_price" : "prix_m2",
 # Cadastre table
 "cad_street_full" : "voie_nom",

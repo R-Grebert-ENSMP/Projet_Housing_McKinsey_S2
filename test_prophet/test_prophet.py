fbd00b675a5af305e6a672ee210d64202a675524
cohennion <56261575+cohennion@users.noreply.github.com>
2020-06-05 16:04:22 +0200

    test_prophet

--- test_prophet/test_prophet.py
+++ test_prophet/test_prophet.py
@@ -2,15 +2,20 @@ import pandas as pd
 from fbprophet import Prophet
 
 #df = pd.read_csv('.../master_table.csv')
-df = pd.read_csv('Projet_Housing_McKinsey_S2/test_prophet/test_years.csv')
+#df = pd.read_csv('Projet_Housing_McKinsey_S2/test_prophet/test_years.csv')
+data = {'ds' : [2014, 2015, 2016, 2017, 2018], 'y' : [1000, 2000, 3000, 4000, 5000]}
+df = pd.DataFrame(data, columns = ['ds', 'y'])
+print(df)
 
-Prophet.fit(df)
-future_years = Prophet.make_future_dataframe(periods = 1, freq = 'year')
-forecast_years = Prophet.predict(future_years)
+m = Prophet()
+m.fit(df)
+future_years = m.make_future_dataframe(periods = 1, freq = 'year')
+future_years.tail()
+#forecast_years = m.predict(future_years)
 
-forecast_years[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+#forecast_years[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
 #comparison with the real fifth year
 from fbprophet.diagnostics import cross_validation
-df_cv = Prophet.cross_validation(horizon = 1, unit = 'years', initial = 4)
+#df_cv = m.cross_validation(horizon = 1, unit = 'years', initial = 4)
 df_cv.head()
